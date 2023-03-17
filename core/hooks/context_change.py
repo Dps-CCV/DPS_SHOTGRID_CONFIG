d208b607467984da5cd3481afb39918eb5234206
Dps-CCV <78370648+Dps-CCV@users.noreply.github.com>
2023-03-17 14:19:25 +0100

    Inicial

---
+++ core/hooks/context_change.py
@@ -0,0 +1,149 @@
+# Copyright (c) 2018 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+"""
+This hook gets executed before and after the context changes in Toolkit.
+"""
+
+from tank import get_hook_baseclass
+
+
+
+class ContextChange(get_hook_baseclass()):
+    """
+    - If an engine **starts up**, the ``current_context`` passed to the hook
+      methods will be ``None`` and the ``next_context`` parameter will be set
+      to the context that the engine is starting in.
+
+    - If an engine is being **reloaded**, in the context of an engine restart
+      for example, the ``current_context`` and ``next_context`` will usually be
+      the same.
+
+    - If a **context switch** is requested, for example when a user switches
+      from project to shot mode in Nuke Studio, ``current_context`` and ``next_context``
+      will contain two different context.
+
+    .. note::
+
+       These hooks are called whenever the context is being set in Toolkit. It is
+       possible that the new context will be the same as the old context. If
+       you want to trigger some behavior only when the new one is different
+       from the old one, you'll need to compare the two arguments using the
+       ``!=`` operator.
+    """
+
+    def pre_context_change(self, current_context, next_context):
+        """
+        Executed before the context has changed.
+
+        The default implementation does nothing.
+
+        :param current_context: The context of the engine.
+        :type current_context: :class:`~sgtk.Context`
+        :param next_context: The context the engine is switching to.
+        :type next_context: :class:`~sgtk.Context`
+        """
+        pass
+
+    def post_context_change(self, previous_context, current_context):
+        import os
+        import sgtk
+        """
+        Executed after the context has changed.
+
+        The default implementation does nothing.
+
+        :param previous_context: The previous context of the engine.
+        :type previous_context: :class:`~sgtk.Context`
+        :param current_context: The current context of the engine.
+        :type current_context: :class:`~sgtk.Context`
+        """
+
+        try:
+            os.environ["PROJECT"] = str(current_context.project["name"])
+            self.logger.info("Environment variable PROJECT changed to %s", str(current_context.project["name"]))
+            arnold_plugin_path = os.path.join(
+                os.environ["PROJECT_PATH"], "CONFIG", "MAYA", "ARNOLD_SHADERS"
+            )
+            os.environ["ARNOLD_PLUGIN_PATH"] += os.pathsep + arnold_plugin_path
+            if current_context.entity:
+                current_engine = sgtk.platform.current_engine()
+                os.environ['SHOTGUN_TASK_TYPE'] = str(current_engine.context.task['type'])
+                os.environ['SHOTGUN_TASK_ID'] = str(current_engine.context.task['id'])
+                os.environ['SHOTGUN_ENTITY_TYPE'] = str(current_engine.context.task['type'])
+                os.environ['SHOTGUN_ENTITY_ID'] = str(current_engine.context.task['id'])
+
+                if current_context.entity["type"] == 'Shot':
+                    os.environ["SHOT"] = current_context.entity["name"]
+                    tk = current_engine.sgtk
+                    template = tk.templates["shot_root"]
+                    fields = current_context.as_template_fields(template)
+                    shot_path = template.apply_fields(fields)
+                    os.environ["SHOT_FOLDER"] = str(shot_path)
+                    self.logger.info("Environment variable SHOT changed to %s", str(current_context.entity["name"]))
+                    self.logger.info("Environment variable SHOT_FOLDER changed to %s", str(shot_path))
+
+                    seq = current_context.sgtk.shotgun.find_one("Shot", [["id", "is", current_context.entity["id"]]], ["sg_sequence", "sg_efecto_a_hacer", "sg_method"])
+                    os.environ["SEQ"] = str(seq["sg_sequence"]["name"])
+                    os.environ["DESCRIPTION"] = str(seq["sg_efecto_a_hacer"])
+                    methods = ''
+                    for i in seq["sg_method"]:
+                        methods += ' ' + i['name'] + ','
+                    os.environ["METHODS"] = methods
+                    self.logger.info("Environment variable SEQ changed to %s", str(seq["sg_sequence"]["name"]))
+
+
+                    clip = current_context.sgtk.shotgun.find_one("Shot", [["id", "is", current_context.entity["id"]]],
+                                                                ["sg_source_clip", "sg_source_clip.SourceClip.sg_lmt"])
+
+                    ###Fill clip and lmt settings if we have those values. If we don't we set empty variables because OCIO configs don't work if there are no env variables created
+                    if clip["sg_source_clip"]:
+                        os.environ["CLIP"] = str(clip["sg_source_clip"]["name"])
+                        self.logger.info("Environment variable CLIP changed to %s", str(clip["sg_source_clip"]["name"]))
+
+                        os.environ["LMT"] = str(clip["sg_source_clip.SourceClip.sg_lmt"])
+                        self.logger.info("Environment variable LMT changed to %s", str(clip["sg_source_clip"]["name"]))
+
+                    else:
+                        os.environ["CLIP"] = " "
+                        os.environ["LMT"] = " "
+
+
+                elif current_context.entity["type"] == 'Asset':
+                    os.environ["ASSET"] = current_context.entity["name"]
+                    self.logger.info("Environment variable ASSET changed to %s", str(current_context.entity["name"]))
+
+                if current_engine._Engine__engine_instance_name == 'tk-nuke':
+                    import nuke
+                    reloadConfig = nuke.root().knob('reloadConfig')
+                    reloadConfig.execute()
+                    self.logger.info("Reload Config %s", str(current_engine._Engine__engine_instance_name))
+
+                elif current_engine._Engine__engine_instance_name == 'tk-maya':
+                    import nuke
+                    reloadConfig = nuke.root().knob('reloadConfig')
+                    reloadConfig.execute()
+                    self.logger.info("Reload Config %s", str(current_engine._Engine__engine_instance_name))
+
+                elif current_engine._Engine__engine_instance_name == 'tk-houdini':
+                    hou.Color.reloadOCIO()
+                    self.logger.info("Reload Config %s", str(current_engine._Engine__engine_instance_name))
+
+            # DPS Write shortcuts for Shotgun Writes
+            if sgtk.platform.current_engine() == "tk-nuke":
+                import sys
+                import os
+                sys.path.append(os.path.dirname(__file__))
+                import WriteShortCuts
+
+                WriteShortCuts.WriteShortCuts()
+        except:
+            pass
+        pass
---
+++ hooks/tk-multi-launchapp/before_app_launch.py
@@ -0,0 +1,225 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+"""
+Before App Launch Hook
+
+This hook is executed prior to application launch and is useful if you need
+to set environment variables or run scripts as part of the app initialization.
+"""
+
+import os
+import sys
+import ConfigParser  # Python 3 issue.
+
+import tank
+
+
+class BeforeAppLaunch(tank.Hook):
+    """
+    Hook to set up the system prior to app launch.
+    """
+
+    def execute(
+        self, app_path, app_args, version, engine_name, software_entity=None, **kwargs
+    ):
+        """
+        The execute function of the hook will be called prior to starting the required application
+
+        :param app_path: (str) The path of the application executable
+        :param app_args: (str) Any arguments the application may require
+        :param version: (str) version of the application being run if set in the
+            "versions" settings of the Launcher instance, otherwise None
+        :param engine_name (str) The name of the engine associated with the
+            software about to be launched.
+        :param software_entity: (dict) If set, this is the Software entity that is
+            associated with this launch command.
+
+        Additional references
+            https://developer.shotgunsoftware.com/624f2593/
+        """
+
+        # accessing the current context (current shot, etc)
+        # can be done via the parent object
+        #
+        # > multi_launchapp = self.parent
+        # > current_entity = multi_launchapp.context.entity
+
+        # you can set environment variables like this:
+        # os.environ["MY_SETTING"] = "foo bar"
+
+        ## Acceder al contexto
+        multi_launchapp = self.parent
+        current_context = multi_launchapp.context
+
+        index = __file__.find("hooks\\")
+        config = __file__[:index]
+        os.environ['CONFIG_FOLDER'] = config
+
+
+        ## Definir variables generales para su posible uso posterior dentro de las aplicaciones
+        os.environ["PROJECT"] = str(current_context.project["name"])
+        os.environ["ARTIST"] = current_context.user["name"]
+        project_path = self.parent.tank.roots.get("primary")
+        os.environ["PROJECT_PATH"] = project_path
+        rawMount = os.path.normpath(project_path)
+        Mount = rawMount.split(os.sep)[0]
+        os.environ["MOUNT"] = Mount
+        ocio_path = os.path.join(
+            project_path, "CONFIG", "COLOR", "ACES_1.2", "config.ocio"
+        )
+        os.environ["OCIO"] = ocio_path
+
+        arnold_plugin_path = os.path.join(
+            project_path, "CONFIG", "MAYA", "ARNOLD_SHADERS"
+        )
+        os.environ["ARNOLD_PLUGIN_PATH"] = arnold_plugin_path
+
+        # filters = [["code" "is" ]]
+        getColor = tank.platform.current_engine().shotgun.find_one("Project", [["name", "is", str(current_context.project["name"])]], ["sg_espacio___color"])
+
+        os.environ["PROJECTCOLORSPACE"] = str(getColor["sg_espacio___color"])
+
+        getMask = tank.platform.current_engine().shotgun.find_one("Project", [
+            ["name", "is", str(current_context.project["name"])]], ["sg_formato___ratio"])
+
+        os.environ["PROJECTMASK"] = str(getMask["sg_formato___ratio"])
+
+        os.environ["RV_SUPPORT_PATH"] = os.path.join(project_path, "CONFIG", "COLOR", "RV")
+
+        getCode = tank.platform.current_engine().shotgun.find_one("Project", [["name", "is", str(current_context.project["name"])]], ["code"])
+
+        os.environ["PROJECT_CODE"] = str(getCode["code"])
+
+        os.environ['RV_USE_CUTS_IN_SCREENING_ROOM'] = "1"
+
+        os.environ['RV_CUSTOM_MATTE_DEFINITIONS'] = os.path.join(project_path, "CONFIG", "COLOR", "RV", "SupportFiles", "custom_mattes")
+
+        os.environ['HOUDINI_ACCESS_METHOD'] = "2"
+
+        os.environ['RV_USE_CUTS_IN_SCREENING_ROOM'] = "True"
+
+
+        os.environ['SHOTGUN_SITE'] = tank.platform.current_engine().sgtk.shotgun_url
+
+        os.environ['SHOTGUN_CONFIG_URI'] = "sgtk:descriptor:path?path=" + tank.platform.current_engine().sgtk.configuration_descriptor.get_path() + "\config"
+        os.environ['SHOTGUN_SGTK_MODULE_PATH'] = tank.platform.current_engine().sgtk.configuration_descriptor.get_path() + "\instal\core\python"
+
+        ###Empty variables fro clip and lmt. Later they are set at context change
+        os.environ["CLIP"] = " "
+        os.environ["LMT"] = " "
+        os.environ["SHOT"] = " "
+
+        ###Variables para Davinci Resolve
+        os.environ[
+            'RESOLVE_SCRIPT_API'] = "C:\\ProgramData\\Blackmagic Design\\DaVinci Resolve\\Support\\Developer\\Scripting"
+        os.environ['RESOLVE_SCRIPT_LIB'] = "C:\\Program Files\\Blackmagic Design\\DaVinci Resolve\\fusionscript.dll"
+        sys.path.append("C:\\ProgramData\\Blackmagic Design\\Support\\Developer\\Scripting\\Modules")
+        tank.util.append_path_to_env_var("PYTHONPATH",
+                                         "C:\\ProgramData\\Blackmagic Design\\DaVinci Resolve\\Support\\Developer\\Scripting\\Modules")
+
+        ## Definir variables especificas de cada aplicacion
+        if engine_name == "tk-maya":
+            ## Variables de Maya
+            # maya_environ_path = os.path.join(
+            #     project_path, "CONFIG/MAYA/2020"
+            # )
+            # os.environ["MAYA_APP_DIR"] = maya_environ_path
+
+            shelves_environ_path = "P:\LIBRERIA\MAYA_SCRIPTS\SHELVES"
+            os.environ["MAYA_SHELF_PATH"] = shelves_environ_path
+
+            ###YETI IMPORTS
+            os.environ['peregrinel_LICENSE'] = '5053@servidor-licencias-api'
+            yeti_path = "P:\\LIBRERIA\\MAYA_PLUGINS\\YETI"
+            yeti_bin_path = "P:\\LIBRERIA\\MAYA_PLUGINS\\YETI\\bin"
+            yeti_plugins_path = "P:\\LIBRERIA\\MAYA_PLUGINS\\YETI\\plug-ins"
+            if "MAYA_MODULE_PATH" in os.environ.keys():
+                os.environ["MAYA_MODULE_PATH"] += os.pathsep + yeti_path
+            else:
+                os.environ["MAYA_MODULE_PATH"] = yeti_path + os.pathsep
+            sys.path.append(yeti_bin_path)
+            self.logger.info(sys.path)
+
+            if "MTOA_EXTENSIONS_PATH" in os.environ.keys():
+                tank.util.append_path_to_env_var("MTOA_EXTENSIONS_PATH", yeti_plugins_path)
+            else:
+                os.environ["MTOA_EXTENSIONS_PATH"] = yeti_plugins_path
+            # os.environ["ARNOLD_PLUGIN_PATH"] = os.environ["ARNOLD_PLUGIN_PATH"] + os.pathsep + yeti_bin_path
+            tank.util.append_path_to_env_var("ARNOLD_PLUGIN_PATH", yeti_bin_path)
+
+            scripts_environ_path = "P:\LIBRERIA\MAYA_SCRIPTS"
+            os.environ["MAYA_SCRIPT_PATH"] = scripts_environ_path
+            if os.environ.get("PYTHONPATH") is not None:
+                os.environ["PYTHONPATH"] += ";" + scripts_environ_path
+                os.environ["PYTHONPATH"] += ";" + scripts_environ_path + "/PYTHON"
+            else:
+                os.environ["PYTHONPATH"] = scripts_environ_path
+                os.environ["PYTHONPATH"] += ";" + scripts_environ_path + "/PYTHON"
+
+            render_environ_path = os.path.abspath(os.path.join(
+                project_path, "CONFIG/MAYA/render_settings/"
+            ))
+            os.environ["MAYA_RENDER_SETUP_GLOBAL_TEMPLATE_PATH"] = render_environ_path
+            os.environ["MAYA_RENDER_SETUP_GLOBAL_PRESETS_PATH"] = render_environ_path
+
+            color_environ_path = os.path.abspath(os.path.join(
+                project_path, "CONFIG/MAYA/color_prefs.xml"
+            ))
+            os.environ["MAYA_COLOR_MANAGEMENT_POLICY_FILE"] = color_environ_path
+
+            os.environ["BIFROST_LIB_CONFIG_FILES"] = 'P:\\LIBRERIA\\MAYA_ASSETS\\BiphostGraphs\\DarePlanetConfigurations.json'
+
+
+        elif engine_name == "tk-nuke":
+            ##Variables de Nuke
+            nuke_environ_path = os.path.join(
+                project_path, "CONFIG/NUKE"
+            )
+            tank.util.append_path_to_env_var("NUKE_PATH", nuke_environ_path)
+
+            # DPS IMPORT OPTICAL FLARES loacted in path P:\LIBRERIA\SCRIPTS
+            os.environ['OPTICAL_FLARES_LICENSE_PATH'] = "P:\\LIBRERIA\\SCRIPTS"
+            os.environ['OPTICAL_FLARES_PRESET_PATH'] = "P:\\LIBRERIA\\SCRIPTS"
+            os.environ['OPTICAL_FLARES_PATH'] = "P:\\LIBRERIA\\SCRIPTS"
+            os.environ['NUKE_INSTALL'] = str(app_path)
+
+            nnflow_path = os.path.join(
+                project_path, "CONFIG/NUKE/GIZMOS/NNFlowVector"
+            )
+            sys.path.append(nnflow_path)
+            tank.util.append_path_to_env_var("NUKE_PATH", nnflow_path)
+            
+
+
+        elif engine_name == "tk-hiero" or engine_name == "tk-nukestudio":
+            hieroPlugin_environ_path = "P:\\LIBRERIA\\HIERO_PLUGIN_PATH"
+            tank.util.append_path_to_env_var("HIERO_PLUGIN_PATH", hieroPlugin_environ_path)
+
+
+        elif engine_name == "tk-houdini":
+            folder = "Houdini " + version
+            houdini_environ_path = os.path.abspath(os.path.join(
+                project_path, "CONFIG/HOUDINI/", folder
+            ))
+            # os.environ['HOUDINI_USER_PREF_DIR'] = houdini_environ_path
+            houdini_dps_plugins_path = os.path.abspath(os.path.join(
+                os.environ['CONFIG_FOLDER'], "bundles", "CONFIG/HOUDINI/", folder
+            ))
+            os.environ['HOUDINI_USER_PREF_DIR'] = houdini_environ_path
+            os.environ['HOUDINI_DPS_PLUGINS'] = houdini_dps_plugins_path
+
+            self.logger.info("Test %s", houdini_environ_path)
+
+
+
+
+
+
