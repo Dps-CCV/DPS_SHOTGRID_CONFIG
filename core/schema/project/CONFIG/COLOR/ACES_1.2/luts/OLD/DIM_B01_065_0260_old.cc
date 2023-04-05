b9b4633c2fa97f43cd3ef23a164db952a41b49db
Dps-CCV <78370648+Dps-CCV@users.noreply.github.com>
2023-04-05 12:55:27 +0200

    added config schema

---
+++ core/schema/project/CONFIG/COLOR/ACES_1.2/luts/OLD/DIM_B01_065_0260_old.cc
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ColorCorrection id="DIM_B01_065_0260">
+    <Description> DIM_B01_065_0260 </Description>
+    <SOPNode>
+        <Slope>1.2793 1.2253 1.1802</Slope>
+        <Offset>-0.1593 -0.1293 -0.1033</Offset>
+        <Power>0.9206 0.9352 0.9697</Power>
+    </SOPNode>
+    <SatNode>
+        <Saturation>1.0000</Saturation>
+    </SatNode>
+</ColorCorrection>
