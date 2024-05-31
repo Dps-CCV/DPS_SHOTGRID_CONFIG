5276ac9bfb28d0e7958e73174564812ec3c70996
Dps-CCV <78370648+Dps-CCV@users.noreply.github.com>
2024-05-31 15:57:29 +0200

    Update updateConfig.py

--- .github/workflows/updateConfig.py
+++ .github/workflows/updateConfig.py
@@ -1,6 +1,11 @@
 def main():
   import shotgun-api3
-
+  import os
+  sg = shotgun_api3.Shotgun("https://dareplanet.shotgunbstudio.com",
+                          script_name="GithubUpdateConfig",
+                          api_key=os.environ['UPDATECONFIGKEY'])
+  data = {'descriptor': "sgtk:descriptor:git_branch?branch=master&path=https://github.com/Dps-CCV/DPS_SHOTGRID_CONFIG.git&version="+os.environ['NEWCODE']}
+  sg.update('PipelineConfiguration', 2215, data)
 
 
 
