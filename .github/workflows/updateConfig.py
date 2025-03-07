1e7be8bf2cfdcf12f859a847b2049828ac034235
Carlos.Cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2025-03-07 17:58:24 +0100

    Nuevo test de github workflows 4

--- .github/workflows/updateConfig.py
+++ .github/workflows/updateConfig.py
@@ -4,8 +4,12 @@ def main():
   sg = shotgun_api3.Shotgun("https://dareplanet.shotgunstudio.com",
                           script_name="GithubUpdateConfig",
                           api_key=os.environ['UPDATECONFIGKEY'])
-  data = {'descriptor': "sgtk:descriptor:git_branch?branch=master&path=https://github.com/Dps-CCV/DPS_SHOTGRID_CONFIG.git&version="+os.environ['NEWCODE']}
-  sg.update('PipelineConfiguration', 2215, data)
+  data = {'descriptor': "sgtk:descriptor:git_branch?branch="+os.environ['BRANCH']+"&path=https://github.com/Dps-CCV/DPS_SHOTGRID_CONFIG.git&version="+os.environ['NEWCODE']}
+  if os.environ['BRANCH'] == "master":
+    id = 2215
+  elif os.environ['BRANCH'] == "22Dogs":
+    id = 2443
+  sg.update('PipelineConfiguration', id, data)
 
 
 
