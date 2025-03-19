e8b24b95a7053c72ef6fc86c2571521fbfc01660
Carlos.Cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2025-03-20 00:39:57 +0100

    Intento arreglo tk-nuke y tk-nuke-writenode 12

--- core/schema/project/CONFIG/NUKE/SCRIPTS/FormatFix.py
+++ core/schema/project/CONFIG/NUKE/SCRIPTS/FormatFix.py
@@ -8,4 +8,4 @@ def fix():
     groupNode = nuke.toNode(group)
     if groupNode.knob('tk_profile_list').value() in ['PRECOMP', 'Render 16bits']:
         writeNode.knob("file_type").setValue(os.environ["FormExt"])
-        writeNode.knob("compression").setValue(os.environ["CompressionExt"])
+        writeNode.knob("compression").setValue(groupNode.knob("compression").value())
