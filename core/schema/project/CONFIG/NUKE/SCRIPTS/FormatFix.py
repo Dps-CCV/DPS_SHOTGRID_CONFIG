cf7bc6d3771982092e1e727d8a6ed8eec10d4ef5
Carlos.Cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2025-03-20 20:51:10 +0100

    latest working

--- core/schema/project/CONFIG/NUKE/SCRIPTS/FormatFix.py
+++ core/schema/project/CONFIG/NUKE/SCRIPTS/FormatFix.py
@@ -2,9 +2,27 @@ import nuke
 import os
 
 
-def fix():
-    writeNode = nuke.thisNode()
-    group = ''.join(nuke.thisNode().fullName().split('.')[:-1])
-    groupNode = nuke.toNode(group)
-    if groupNode.knob("_promoted_0").value() != None:
-        writeNode.knob("compression").setValue(groupNode.knob("_promoted_0").value())
+class FormatFixing:
+    def fix(self):
+        print("aquí")
+        writeNode = nuke.thisNode()
+        group = ''.join(nuke.thisNode().fullName().split('.')[:-1])
+        groupNode = nuke.toNode(group)
+        if groupNode.knob("profile_name").getValue() in ['PRECOMP', 'TECH_PRECOMP', 'Render 16bits', 'IMAGE_PLANE', 'ALPHA']:
+            print("hilllo")
+            print(groupNode.knobs())
+            print(groupNode.knob("_promoted_0").getValue())
+            print(groupNode["_promoted_0"].value())
+            print(groupNode.knob("tk_file_type").getValue())
+            print(groupNode.knob("profile_name").getValue())
+            #for key, value in groupNode.knobs().items():
+            #    print("key " + key)
+            #    print("value " + str(groupNode.knob(key).getValue()))
+            texto = groupNode.knob("tk_file_type_settings").getValue()
+            indice = texto.find("Vcompression")+17
+            nuevotexto = texto[indice:]
+            nuevoindice = nuevotexto.find("\n")
+            compr = nuevotexto[:nuevoindice]
+            print(compr)
+            writeNode.knob("file_type").setValue(groupNode.knob("tk_file_type").getValue())
+            writeNode.knob("compression").setValue(compr)
