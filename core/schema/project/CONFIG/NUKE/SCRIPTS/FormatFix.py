fd5a46ba4df03be84395c6a74a2ce04e17bb3bcd
Carlos.Cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2025-03-20 04:51:08 +0100

    promote compression en perfil none y test de writendode

--- core/schema/project/CONFIG/NUKE/SCRIPTS/FormatFix.py
+++ core/schema/project/CONFIG/NUKE/SCRIPTS/FormatFix.py
@@ -6,6 +6,5 @@ def fix():
     writeNode = nuke.thisNode()
     group = ''.join(nuke.thisNode().fullName().split('.')[:-1])
     groupNode = nuke.toNode(group)
-    if groupNode.knob('tk_profile_list').value() in ['PRECOMP', 'Render 16bits']:
-        writeNode.knob("file_type").setValue(os.environ["FormExt"])
-        writeNode.knob("compression").setValue(groupNode.knob("compression").value())
+    if groupNode.knob("_promoted_0").value() != None:
+        writeNode.knob("compression").setValue(groupNode.knob("_promoted_0").value())
