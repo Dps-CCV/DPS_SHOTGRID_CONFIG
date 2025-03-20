67d2891dc8f1a769ae24a5f23d7ba672fe9df562
Carlos.Cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2025-03-20 11:11:54 +0100

    pipeline_configuration_init

--- core/hooks/pipeline_configuration_init.py
+++ core/hooks/pipeline_configuration_init.py
@@ -27,7 +27,7 @@ class PipelineConfigurationInit(Hook):
         index = __file__.find("core\\")
         config = __file__[:index]
         os.environ['CONFIG_FOLDER'] = config
-        #os.environ["FormExt"] = 'exr'
-        #os.environ["CompressionExt"] = 'none'
+        os.environ["FormExt"] = 'exr'
+        os.environ["CompressionExt"] = 'none'
 
         pass
