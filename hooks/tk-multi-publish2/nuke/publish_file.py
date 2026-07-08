38d88bedf3b0c36655c7f3047beb765070efb343
Dps-CCV <78370648+Dps-CCV@users.noreply.github.com>
2026-07-08 16:02:52 +0200

    Add step_name to work_fields in publish_file.py

--- hooks/tk-multi-publish2/nuke/publish_file.py
+++ hooks/tk-multi-publish2/nuke/publish_file.py
@@ -836,6 +836,7 @@ class BasicFilePublishPlugin(HookBaseClass):
             return
 
         work_fields = work_template.get_fields(work_files[0])
+        work_fields['step_name'] = self.parent.context.step['name'].lower()
 
         missing_keys = publish_template.missing_keys(work_fields)
 
@@ -955,6 +956,7 @@ class BasicFilePublishPlugin(HookBaseClass):
         if work_template:
             if work_template.validate(path):
                 work_fields = work_template.get_fields(path)
+                work_fields['step_name'] = self.parent.context.step['name'].lower()
 
         # if we have template and fields, use them to determine the version info
         if work_fields and "version" in work_fields:
