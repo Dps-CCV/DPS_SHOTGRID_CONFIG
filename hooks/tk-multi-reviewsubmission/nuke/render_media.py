ceb322c445c1ea25fc0c2d06a930c51e3b803b44
Dps-CCV <78370648+Dps-CCV@users.noreply.github.com>
2026-07-10 13:36:58 +0200

    Refactor codec settings for media rendering

--- hooks/tk-multi-reviewsubmission/nuke/render_media.py
+++ hooks/tk-multi-reviewsubmission/nuke/render_media.py
@@ -345,26 +345,12 @@ class RenderMedia(HookBaseClass):
         :rtype:                 dict
         """
         settings = {}
-        if sgtk.util.is_windows() or sgtk.util.is_macos():
-            settings["file_type"] = "mov"
-            if nuke.NUKE_VERSION_MAJOR >= 9:
-                # Nuke 9.0v1 changed the codec knob name to meta_codec and added an encoder knob
-                # (which defaults to the new mov64 encoder/decoder).
-                settings["meta_codec"] = "jpeg"
-                settings["mov64_quality_max"] = "3"
-            else:
-                settings["codec"] = "jpeg"
-
-        elif sgtk.util.is_linux():
-            if nuke.NUKE_VERSION_MAJOR >= 9:
-                # Nuke 9.0v1 removed ffmpeg and replaced it with the mov64 writer
-                # http://help.thefoundry.co.uk/nuke/9.0/#appendices/appendixc/supported_file_formats.html
-                settings["file_type"] = "mov64"
-                settings["mov64_codec"] = "jpeg"
-                settings["mov64_quality_max"] = "3"
-            else:
-                # the 'codec' knob name was changed to 'format' in Nuke 7.0
-                settings["file_type"] = "ffmpeg"
-                settings["format"] = "MOV format (mov)"
+        settings["file_type"] = "mov"
+        settings["mov64_codec"] = "AVdn"
+        settings["mov64_fps"] = nuke.root()['fps'].value()
+        settings["mov_h264_codec_profile"] = "High 4:2:0 8-bit"
+        settings["mov64_quality"] = "High"
+        settings["mov64_fast_start"] = True
+        settings["mov64_write_timecode"] = True
 
         return settings
