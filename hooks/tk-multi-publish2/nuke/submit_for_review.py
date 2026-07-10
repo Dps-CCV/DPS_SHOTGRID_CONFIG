9cf123b7cc73772ce196f22747a9f4b095c8a9a5
Dps-CCV <78370648+Dps-CCV@users.noreply.github.com>
2026-07-10 13:35:04 +0200

    Remove step_name assignment from render path fields
    Removed unnecessary assignment of step_name from context.

--- hooks/tk-multi-publish2/nuke/submit_for_review.py
+++ hooks/tk-multi-publish2/nuke/submit_for_review.py
@@ -1,4 +1,4 @@
-﻿# Copyright (c) 2017 Shotgun Software Inc.
+# Copyright (c) 2017 Shotgun Software Inc.
 #
 # CONFIDENTIAL AND PROPRIETARY
 #
@@ -286,7 +286,6 @@ class NukeSubmitForReviewPlugin(HookBaseClass):
 
         render_path_fields = render_template.get_fields(render_path)
         render_path_fields['name'] = sg_publish_data["code"]
-        render_path_fields['step_name'] = self.parent.context.step['name']
         first_frame = item.properties.get("first_frame")
         last_frame = item.properties.get("last_frame")
         colorspace = item.properties.get("color_space")
